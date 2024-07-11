932a4204df56f9d6cac42bc9e8335e60fdf8813d
StephanieHerr <stephanie.herr@red-gate.com>
2024-07-11 17:50:43 -0400

    Adding shortname to widgets table

--- schema-model/Tables/dbo.Widgets.sql
+++ schema-model/Tables/dbo.Widgets.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[Widgets]
 (
 [RecordID] [int] NOT NULL IDENTITY(1, 1),
-[Description] [varchar] (50) NULL
+[Description] [varchar] (50) NULL,
+[ShortName] [varchar] (5) NULL
 )
 GO
 ALTER TABLE [dbo].[Widgets] ADD CONSTRAINT [PK_Widgets] PRIMARY KEY NONCLUSTERED ([RecordID])
