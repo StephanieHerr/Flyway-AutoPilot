f60bea5e527dd8ce47ee1013385cd4fd08b5a18e
StephanieHerr <stephanie.herr@red-gate.com>
2025-01-29 09:56:50 -0500

    Update dbo.Stephanie.sql

--- schema-model/Tables/dbo.Stephanie.sql
+++ schema-model/Tables/dbo.Stephanie.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Stephanie]
 (
-[ID] [int] NOT NULL
+[ID] [int] NOT NULL,
+  Name NVARCHAR(50)
 )
 GO
 ALTER TABLE [dbo].[Stephanie] ADD CONSTRAINT [PK__Stephani__3214EC2715EFA871] PRIMARY KEY CLUSTERED ([ID])
